27a3e8da8b05a519eaf7acb1818e5dbd8c8d8c80
codingKingChobo <ash1388@naver.com>
2023-02-14 13:45:27 +0900

    o

--- algorithm/algorithm/b_10986.cpp
+++ algorithm/algorithm/b_10986.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 vector<vector<int>> sum(1000010);
-
+queue<int> sum1;
+queue<int>tmp;
 int n, m, cnt = 0;
 
 
@@ -18,19 +20,37 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		cin >> num;
 		sum[i].push_back(num);
+		sum1.push(num);
 		if (num % m == 0)cnt++;
 	}
 
-	for (int i = 1; i < n; i++) {
-		for (int k = 0; k < sum[i].size(); k++) {
-			int tmp;
-			sum[i + 1].push_back(sum[i][k] + sum[i + 1][0]);
-			//	cout << sum[i][k] << " + " << sum[i + 1][0] << "="<< sum[i][k] + sum[i + 1][0]<<' ';
-			tmp = sum[i][k] + sum[i + 1][0];
-			if (tmp % m == 0) cnt++;
+
+
+	while (1) {
+		int rpt;
+		tmp.push(sum1.front());
+		sum1.pop();
+		rpt = tmp.size();
+		cout << "rpt : " << rpt << " \n";
+		while (rpt--) {
+			tmp.push(tmp.front() + sum1.front());
+			cout << "sum : " << tmp.front() + sum1.front() << " \n";
+			if ((tmp.front() + sum1.front()) % m == 0) cnt++;
+			tmp.pop();
 		}
-		//	cout << '\n';
+
+		if (sum1.size() == 1) break;
 	}
+	//for (int i = 1; i < n; i++) {
+	//	for (int k = 0; k < sum[i].size(); k++) {
+	//		int tmp;
+	//		sum[i + 1].push_back(sum[i][k] + sum[i + 1][0]);
+	//		cout << sum[i][k] << " + " << sum[i + 1][0] << " = "<< sum[i][k] + sum[i + 1][0]<<' ';
+	//		tmp = sum[i][k] + sum[i + 1][0];
+	//		if (tmp % m == 0) cnt++;
+	//	}
+	//		cout << '\n';
+	//}
 
 	cout << cnt;
 	return 0;
